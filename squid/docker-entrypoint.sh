7a82392a63abb477a43fe9955877711db3338599
lshnls <lshnls@ya.ru>
2026-03-15 21:23:28 +0300

    modified:   README.md 	modified:   docker-compose.yml 	modified:   squid/docker-entrypoint.sh 	modified:   squid/config/squid.conf 	modified:   squid/docker-entrypoint.sh

--- squid/docker-entrypoint.sh
+++ squid/docker-entrypoint.sh
@@ -5,11 +5,12 @@ CERT_DIR=/etc/squid/ssl
 
 mkdir -p "$CERT_DIR" 
 
-
-openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 \
-  -subj "/C=XX/O=Squid Proxy/CN=Squid MITM CA" \
-  -keyout "$CERT_DIR/squidCA.key" \
-  -out "$CERT_DIR/squidCA.crt"
+if [ ! -f "$CERT_DIR/squidCA.key" ] || [ ! -f "$CERT_DIR/squidCA.crt" ]; then
+  openssl req -new -newkey rsa:4096 -days 3650 -nodes -x509 \
+    -subj "/C=XX/O=Squid Proxy/CN=Squid MITM CA" \
+    -keyout "$CERT_DIR/squidCA.key" \
+    -out "$CERT_DIR/squidCA.crt"
+fi
 
   #cat "$CERT_DIR/squidCA.crt" "$CERT_DIR/squidCA.key" > "$CERT_DIR/squidCA.pem"
   chmod 600 "$CERT_DIR/squidCA.key" "$CERT_DIR/squidCA.crt"
